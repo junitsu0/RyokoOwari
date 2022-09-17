2b48853285c150ebb2f504c1450d2f8a72c8abc2
Justin <junitsu00@gmail.com>
2022-09-17 14:33:17 -0500

    converting signup form and home page text

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__) 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'suck-it-trebek'
+
 
 from . import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-
+from app.forms import SignUpForm
 
 @app.route('/')
 def index():
@@ -9,10 +9,11 @@ def index():
         'username': 'justin',
         'email': 'jumanjiro@gmail.com'
     }
-    colors = ['x', 'y', 'z', 'a', 'b', 'c']
+    colors = ['Blackjack', 'Baccarat', 'Roulette', 'Craps', 'Three Card Poker', 'Four Card Poker', "Ultimate Texas Hold'em"]
     return render_template('index.html', user=user_info, colors=colors)
 
 
-@app.route('/test')
-def test():
-    return 'This is a test'
+@app.route('/signup')
+def signup():
+    form = SignUpForm()
+    return render_template('signup.html', form=form)
