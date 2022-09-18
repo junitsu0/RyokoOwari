290cd83c159418b08a9a697aa0a8a888906f0897
Justin <junitsu00@gmail.com>
2022-09-17 21:22:19 -0500

    not late enough to quit

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash
+
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,3 +10,12 @@ class User(db.Model):
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.password = generate_password_hash(kwargs['password'])
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<User {self.id} | {self.username}>"
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from app.forms import SignUpForm
+from app.models import User
 
 @app.route('/')
 def index():
@@ -18,5 +19,14 @@ def signup():
     form = SignUpForm()
     if form.validate_on_submit():
         print('Form has been validated! Yippy Kai Yay')
-        print(form.email.data, form.username.data, form.password.data)
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        existing_user = User.query.filter((User.email == email) | (User.username == username)).first()
+        if existing_user:
+            flash('A user with that username or email already exists.', 'danger')
+            return redirect(url_for('signup'))
+        new_user = User(email=email, username=username, password=password)
+        flash(f"{new_user.username} has been created.", "success")
+        return redirect(url_for('index'))
     return render_template('signup.html', form=form)
