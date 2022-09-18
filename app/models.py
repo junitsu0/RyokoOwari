af352dd79996f97bdae2a23a7a8187d070e2feff
Justin <junitsu00@gmail.com>
2022-09-17 22:03:19 -0500

    MORE DOTS

--- app/models.py
+++ app/models.py
@@ -3,13 +3,13 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash
 
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), nullable=False, unique=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -19,3 +19,20 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.id} | {self.username}>"
+
+# dont need posting =========
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String(255), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<Post {self.id} | {self.title}>"
+# end dont need posting ======
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template, redirect, url_for, flash
-from app.forms import SignUpForm
-from app.models import User
+from app.forms import SignUpForm, PostForm
+from app.models import User, Post
 
 @app.route('/')
 def index():
@@ -26,7 +26,24 @@ def signup():
         if existing_user:
             flash('A user with that username or email already exists.', 'danger')
             return redirect(url_for('signup'))
+            
         new_user = User(email=email, username=username, password=password)
         flash(f"{new_user.username} has been created.", "success")
         return redirect(url_for('index'))
     return render_template('signup.html', form=form)
+
+# don't need posting
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        body = form.body.data
+        new_post = Post(title=title, body=body, user_id=1)
+        flash(f'{new_post.title} has been created.', 'secondary')
+        return redirect(url_for('index'))
+
+    return render_template('createpost.html', form=form)
+
+
+# end dont need posting
